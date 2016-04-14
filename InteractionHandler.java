e61daaa385c28f7e1a6c7e402150743230295cca
Andrew Emerson <Andrew Emerson>
2016-04-14 18:53:16 -0400

    added function to determine interactions based on time/distance limits

--- InteractionHandler.java
+++ InteractionHandler.java
@@ -2,20 +2,34 @@ import java.text.*;
 import java.util.Date;
 
 public class InteractionHandler {
-
+	double PHYSICAL_DISTANCE_LIMIT = 0.1;	//in km
+	long TIME_DIFFERENCE_LIMIT = 120;		//in seconds
+	
+	/**
+	 * Determines if the time difference and physical distance of
+	 * two location logs are within a given range.
+	 */
+	public boolean isInteraction(double distance, long timeDifference) {
+		boolean interaction = false;
+		if ((distance < PHYSICAL_DISTANCE_LIMIT) && (timeDifference < TIME_DIFFERENCE_LIMIT)) {
+			interaction = true;
+		}
+		return interaction;
+	}
+	
 	/**
 	 * Calculates the distance in km between two lat/long points
 	 * using the haversine formula.
 	 */
 	public double haversine(double lat1, double long1, double lat2, double long2){
-	    int r = 6371; // average radius of the earth in km
-	    double dLat = Math.toRadians(lat2 - lat1);
-	    double dLong = Math.toRadians(long2 - long1);
-	    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	    int earthRadius = 6371; 	// average in km
+	    double differenceLat = Math.toRadians(lat2 - lat1);
+	    double differenceLong = Math.toRadians(long2 - long1);
+	    double a = Math.sin(differenceLat / 2) * Math.sin(differenceLat / 2) +
 	       		Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) 
-	      		* Math.sin(dLong / 2) * Math.sin(dLong / 2);
+	      		* Math.sin(differenceLong / 2) * Math.sin(differenceLong / 2);
 	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	    double d = r * c;
+	    double d = earthRadius * c;
 	    return d;
 	}
 	
